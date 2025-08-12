da2a9b4492918d44d6b9f5fd0dada5a1bbb3a370
AKCybulska <akaszubacybulska@gmail.com>
2025-08-12 17:57:18 +0200

    styles:slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,48 +2,31 @@ import $ from 'jquery';
 import 'slick-carousel';
 import '../less/main.less';
 import { loadCartItems } from './cart.js';
+// import 'slick-carousel/slick/slick.min.js';
 
 $(document).ready(function () {
   /* gorny slider */
+  if (!$('.slider__container').hasClass('slick-initialized')) {
   $('.slider__container').slick({
     arrows: true,
     dots: true,
     lazyLoad: 'ondemand',
-prevArrow: `<button type="button" class="slick-prev">
-    <svg viewBox="0 0 20 20">
-      <path d="M13.41 15.41L8.83 10.83L13.41 6.25L12 4.83L6 10.83L12 16.83L13.41 15.41Z" />
-    </svg>
-  </button>`,
-  nextArrow: `<button type="button" class="slick-next">
-    <svg viewBox="0 0 20 20">
-      <path d="M6.59 15.41L11.17 10.83L6.59 6.25L8 4.83L14 10.83L8 16.83L6.59 15.41Z" />
-    </svg>
-  </button>`
   });
-
+  }
 /* promo slider */
+if (!$('.promotions__slider').hasClass('slick-initialized')) {
 $('.promotions__slider').slick({
-  slidesToShow: 4, // domyślnie 4 slajdy na desktop
+  slidesToShow: 5, // domyślnie 4 slajdy na desktop
   slidesToScroll: 1,
-  infinite: false,
+  infinite: true,
   arrows: true,
   dots: true,
-    lazyLoad: 'ondemand',
-  prevArrow: `<button type="button" class="slick-prev">
-    <svg viewBox="0 0 20 20">
-      <path d="M13.41 15.41L8.83 10.83L13.41 6.25L12 4.83L6 10.83L12 16.83L13.41 15.41Z" />
-    </svg>
-  </button>`,
-  nextArrow: `<button type="button" class="slick-next">
-    <svg viewBox="0 0 20 20">
-      <path d="M6.59 15.41L11.17 10.83L6.59 6.25L8 4.83L14 10.83L8 16.83L6.59 15.41Z" />
-    </svg>
-  </button>`,
+  lazyLoad: 'ondemand',
   responsive: [
     {
       breakpoint: 1024, // tablet
       settings: {
-        slidesToShow: 3
+        slidesToShow: 4
       }
     },
     {
@@ -55,6 +38,7 @@ $('.promotions__slider').slick({
     }
   ]
 });
+}
 
 
 
